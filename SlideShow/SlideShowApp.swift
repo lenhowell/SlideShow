5874215cdf3fd4378e16670e592a47ee29228fde
Lenard Howell <len.howell@gmail.com>
2025-01-23 16:05:37 -0500

    Initial Commit

---
+++ SlideShow/SlideShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideShowApp.swift
+//  SlideShow
+//
+//  Created by Lenard Howell on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SlideShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlideShowTests/SlideShowTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlideShowTests.swift
+//  SlideShowTests
+//
+//  Created by Lenard Howell on 1/23/25.
+//
+
+import Testing
+@testable import SlideShow
+
+struct SlideShowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
